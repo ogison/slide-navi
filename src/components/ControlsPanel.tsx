136c1b5a977dc1e9f41eb904957c1888cf02e4ab
ogison <meyo.ogs9@gmail.com>
2025-09-23 21:35:44 +0900

    feat: add icon switching feature

--- src/components/ControlsPanel.tsx
+++ src/components/ControlsPanel.tsx
@@ -3,21 +3,7 @@ import { ChangeEvent } from "react";
 import type { SlideImage } from "../types/slides";
 
 import styles from "./ControlsPanel.module.scss";
-
-const SCRIPT_PLACEHOLDER = `# タイトル
-@Dr. Hikari: こんにちは
-よろしくおねがいします >>2s
-
-# 目次
-今回紹介するのは
-・入門編
-・応用編
-・Q&A >>click
-
-# 入門編
-まずAのポイントは3つあります。
-1つめは…`;
-
+import { SCRIPT_PLACEHOLDER } from "@/constants";
 
 type ControlsPanelProps = {
   onPdfUpload: (event: ChangeEvent<HTMLInputElement>) => void;
